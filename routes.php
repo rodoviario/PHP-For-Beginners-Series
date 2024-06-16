ea172c08c2af995dbf5f51fd35282682ee9292de
Rodolfo Guagnini <rodolfo.guagnini@gmail.com>
2024-06-16 15:31:36 -0300

    ep33: broken

--- routes.php
+++ routes.php
@@ -1,10 +1,13 @@
 <?php
 
-return [
-    '/' => 'controllers/index.php',
-    '/about' => 'controllers/about.php',
-    '/notes' => 'controllers/notes/index.php',
-    '/note' => 'controllers/notes/show.php',
-    '/notes/create' => 'controllers/notes/create.php',
-    '/contact' => 'controllers/contact.php',
-];
+// return [
+//     '/' => 'controllers/index.php',
+//     '/about' => 'controllers/about.php',
+//     '/notes' => 'controllers/notes/index.php',
+//     '/note' => 'controllers/notes/show.php',
+//     '/notes/create' => 'controllers/notes/create.php',
+//     '/contact' => 'controllers/contact.php',
+// ];
+
+$router->get('/', 'controllers/index.php');
+$router->delete('/note', 'controller/notes/destroy.php');
