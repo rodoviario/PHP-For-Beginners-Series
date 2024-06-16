c4f411f3a74e9b058de54b35a31e4d6bd175bd53
Rodolfo Guagnini <rodolfo.guagnini@gmail.com>
2024-06-16 17:17:36 -0300

    ep35: working

--- controllers/notes/destroy.php
+++ controllers/notes/destroy.php
@@ -1,9 +1,12 @@
 <?php
 
+use Core\App;
 use Core\Database;
 
-$config = require base_path('config.php');
-$db = new Database($config['database']);
+// $db = App::container()->resolve('Core\Database');
+// $db = App::container()->resolve(Database::class);
+// $db = App::container()->resolve(\Core\Database::class);
+$db = App::resolve(Database::class);
 
 $currentUserId = 1;
 
