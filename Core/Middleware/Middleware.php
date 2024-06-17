05afec4cb47790e0edaafc9bf62f6a1f5615ccbb
Rodolfo Guagnini <rodolfo.guagnini@gmail.com>
2024-06-17 19:49:37 -0300

    feat: improved middleware logic

--- Core/Middleware/Middleware.php
+++ Core/Middleware/Middleware.php
@@ -4,8 +4,23 @@ namespace Core\Middleware;
 
 class Middleware
 {
-    const MAP = [
+    public const MAP = [
         'guest' => Guest::class,
         'auth' => Auth::class
     ];
+
+    public static function resolve($key)
+    {
+        if (!$key) {
+            return;
+        }
+
+        $middleware = static::MAP[$key] ?? false;
+    
+        if (!$middleware) {
+            throw new \Exception("No matching middleware found for key '{$key}'.");
+        }
+
+        (new $middleware)->handle();
+    }
 }
