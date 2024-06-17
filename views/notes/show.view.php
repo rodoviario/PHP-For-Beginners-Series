be0ec1782df2b4430c26def7487f746e9b8af6f0
Rodolfo Guagnini <rodolfo.guagnini@gmail.com>
2024-06-17 09:36:22 -0300

    feat: edit and cancel actions

--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -10,11 +10,15 @@
 
         <p><?= htmlspecialchars($note['body']) ?></p>
 
-        <form class="mt-6" method="POST">
+        <footer class="mt-6">
+            <a href="/note/edit?id=<?= $note['id'] ?>" class="text-gray-500 border border-current px-3 py-1 rounded">Edit</a>
+        </footer>
+
+        <!-- <form class="mt-6" method="POST">
             <input type="hidden" name="_method" value="DELETE">
             <input type="hidden" name="id" value="<?= $note['id'] ?>">
             <button class="text-sm text-red-500">Delete</button>
-        </form>
+        </form> -->
     </div>
 </main>
 
