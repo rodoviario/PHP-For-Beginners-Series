02e131cd7293302e67fd70e56cb37583df4124a7
Rodolfo Guagnini <rodolfo.guagnini@gmail.com>
2024-06-19 17:43:56 -0300

    wip

--- Http/Forms/LoginForm.php
+++ Http/Forms/LoginForm.php
@@ -8,17 +8,33 @@ class LoginForm
 {
     protected $errors = [];
 
-    public function validate($email, $password)
+    public function __construct($attributes)
     {
-        if (!Validator::email($email)) {
+        if (!Validator::email($attributes['email'])) {
             $this->errors['email'] = 'Please provide a valid email address.';
         }
-
-        if (!Validator::string($password)) {
+    
+        if (!Validator::string($attributes['password'])) {
             $this->errors['password'] = 'Please provide a valid password.';
         }
 
-        return empty($this->errors);
+    }
+
+    public static function validate($attributes)
+    {
+        $instance = new static($attributes);
+
+        if($instance->failed()) {
+            throw new ValidationException();
+        }
+
+        // el form no tiene errores
+        return $instance;
+    }
+    
+    public function failed()
+    {
+        return count($this->errors);
     }
 
     // agregar un getter para sacar los errores si los hubiera
--- Http/controllers/session/store.php
+++ Http/controllers/session/store.php
@@ -4,18 +4,18 @@ use Core\Authenticator;
 use Core\Session;
 use Http\Forms\LoginForm;
 
-$email = $_POST['email'];
-$password = $_POST['password'];
+$form = LoginForm::validate($attributes = [
+    'email' => $_POST['email'],
+    'password' => $_POST['password']
+]);
 
-$form = new LoginForm();
 
-if ($form->validate($email, $password)) {
-    if ((new Authenticator)->attempt($email, $password)) {
-        redirect('/');
-    }
-    
-    $form->error('email', 'No matching account for that email address and password.');
+if ((new Authenticator)->attempt($attributes['email'], $attributes['password'])) {
+    redirect('/');
 }
+
+$form->error('email', 'No matching account for that email address and password.');
+
 
 Session::flash('errors',$form->errors());
 Session::flash('old', [
