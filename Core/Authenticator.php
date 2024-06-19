b697d098c837391c4e3e977e78555b44b88c9647
Rodolfo Guagnini <rodolfo.guagnini@gmail.com>
2024-06-19 09:05:06 -0300

    refactor: session core class

--- Core/Authenticator.php
+++ Core/Authenticator.php
@@ -29,24 +29,11 @@ class Authenticator
             'email' => $user['email']
         ];
 
-        // esta es una buena práctica... regenerar el session_id
         session_regenerate_id(true);
     }
 
     public function logout()
     {
-        $_SESSION = [];
-        session_destroy();
-
-        $params = session_get_cookie_params();
-        setcookie(
-            'PHPSESSID',
-            '',
-            time() - 3600,
-            $params['path'],
-            $params['domain'],
-            $params['secure'],
-            $params['httponly']
-        );
+        Session::destroy();
     }
 }
